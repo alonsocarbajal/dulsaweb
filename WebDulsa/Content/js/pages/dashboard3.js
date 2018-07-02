acf28ab33a217ae511ef815686be36a915d565fb
alonso <alonsocarbajal@hotmail.com>
2018-07-02 13:52:58 -0600

    cambios de 02.julio

---
+++ WebDulsa/Content/js/pages/dashboard3.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('table').DataTable({
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
+        }});
+});
---
+++ WebDulsa/Controllers/AsesoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Modelo;
+
+namespace WebDulsa.Controllers
+{
+    public class AsesoresController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Asesores
+        public ActionResult Index()
+        {
+            return View(db.Asesores.ToList());
+        }
+
+        // GET: Asesores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asesor asesor = db.Asesores.Find(id);
+            if (asesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asesor);
+        }
+
+        // GET: Asesores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Asesores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Nombre,ApellidoPaterno,ApellidoMaterno,Telefono")] Asesor asesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Asesores.Add(asesor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(asesor);
+        }
+
+        // GET: Asesores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asesor asesor = db.Asesores.Find(id);
+            if (asesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asesor);
+        }
+
+        // POST: Asesores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Nombre,ApellidoPaterno,ApellidoMaterno,Telefono")] Asesor asesor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(asesor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(asesor);
+        }
+
+        // GET: Asesores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Asesor asesor = db.Asesores.Find(id);
+            if (asesor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(asesor);
+        }
+
+        // POST: Asesores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Asesor asesor = db.Asesores.Find(id);
+            db.Asesores.Remove(asesor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
